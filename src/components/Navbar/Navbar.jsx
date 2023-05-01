2c2295341ef42ea75ed5120299cddefdc22c9ff3
Nomi Rajpoot <numanrajpoot62@gmail.com>
2023-05-01 22:38:08 +0500

    first-commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar  = () =>{
+return(<>
+<div className="fixed w-full h-20 shadow-xl z-[100]">
+    <div className="d-flex justify-between items-center w-full px-2 2xl:px-16">
+
+    </div>
+</div>
+</>)
+}
+module.exports = Navbar
